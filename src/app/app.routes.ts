5d1edecb1270bf8629ae3bfb4634237d1ed0a80a
AlfredoRiveraaa <alfrexri550@gmail.com>
2025-10-20 22:49:02 -0600

    Se configuró el enrutamiento principal y layout del dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,50 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+// Importa tus componentes
+import { LandingComponent } from './screens/landing/landing.component';
+import { LoginComponent } from './screens/auth/login/login.component';
+import { RegisterComponent } from './screens/auth/register/register.component';
+import { DashboardComponent } from './screens/dashboard/dashboard.component';
+import { HomeComponent } from './screens/dashboard/home/home.component';
+
+export const routes: Routes = [
+  // --- RUTAS PÚBLICAS ---
+  // La Landing Page será la ruta raíz
+  {
+    path: '',
+    component: LandingComponent
+  },
+  // Rutas de Autenticación
+  {
+    path: 'auth/login',
+    component: LoginComponent
+  },
+  {
+    path: 'auth/register',
+    component: RegisterComponent
+  },
+
+  // --- RUTAS PRIVADAS (Protegidas) ---
+  // Usamos el DashboardComponent como layout principal
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    // Estas son las "Rutas Hijas" que se cargarán dentro del <router-outlet> del Dashboard
+    children: [
+      {
+        path: '', // Esta es la ruta por defecto (ej. /dashboard)
+        component: HomeComponent
+      },
+      // Aquí irán las rutas de Persona 2 y 3 (productos, reportes, etc.)
+      // { path: 'products', component: ProductsComponent },
+      // { path: 'reports', component: ReportsComponent },
+    ]
+  },
+
+  // --- REDIRECCIÓN ---
+  // Cualquier otra ruta que no exista, redirige a la Landing Page
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
--- src/app/screens/dashboard/dashboard.component.ts
+++ src/app/screens/dashboard/dashboard.component.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
 
+import { RouterModule } from '@angular/router';
+import { NavbarComponent } from '../../partials/navbar/navbar.component';
+import { SidebarComponent } from '../../partials/sidebar/sidebar.component';
+
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  standalone: true,
+
+  imports: [
+    RouterModule,
+    NavbarComponent,
+    SidebarComponent
+  ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
