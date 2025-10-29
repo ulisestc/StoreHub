36569fad8581c9c80dae5428cd13d0d68aa7e233
AlfredoRiveraaa <alfrexri550@gmail.com>
2025-10-29 09:48:21 -0600

    Se implementó JwtInterceptor y se registró HttpClient

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,22 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
+// Se importa lo necesario para HTTP y los interceptors
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { jwtInterceptor } from './shared/interceptors/jwt.interceptor';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+
+    // Se registra el proveedor de HttpClient
+    //    y se le indica que use el interceptor
+    provideHttpClient(
+      withInterceptors([
+        jwtInterceptor
+      ])
+    )
+  ]
 };
--- src/app/shared/interceptors/jwt.interceptor.ts
+++ src/app/shared/interceptors/jwt.interceptor.ts
@@ -1,5 +1,31 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
 
 export const jwtInterceptor: HttpInterceptorFn = (req, next) => {
+
+  // Se inyecta el servicio de autenticación
+  const authService = inject(AuthService);
+
+  // Se obtiene el token
+  const authToken = authService.getToken();
+
+  // Se verifica si tenemos un token
+  if (authToken) {
+    // Si hay token, se clona la petición y se le añade la cabecera
+    // 'Authorization' con el 'Bearer token'.
+    // El backend (Django) esperará esta cabecera.
+    const clonedReq = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${authToken}`
+      }
+    });
+
+    // Dejamos que la petición clonada (y modificada) continúe
+    return next(clonedReq);
+  }
+
+  // Si no hay token, dejamos que la petición original continúe
+  // (esto es para peticiones públicas como Login o Registro)
   return next(req);
 };
