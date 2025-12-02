a9898c6ad4de98be4aba7fda4e628d3a96daa73c
Johan Yuri Martínez García <johanyuri24@gmail.com>
2025-12-02 09:07:44 -0600

    Se inicio desarrollo del footer con el archivo .ts del partials agregando variable para que la fecha se actualice automáticamente

--- src/app/partials/footer/footer.component.ts
+++ src/app/partials/footer/footer.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'; //  Utilizamos el modulo common para fechas y directivas, esto hace que se actualicen solas
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule
+  ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
-
+  // Aqui ocupamos la fecha actual para el footer
+  currentYear: number = new Date().getFullYear();
 }
